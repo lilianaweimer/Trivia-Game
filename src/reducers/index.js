109a4838eee8841c6784708715496d0604085e7e
Kyle Wong <kylewong2510@gmail.com>
2020-07-22 10:16:54 -0600

    Add reducer names to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,12 @@
 import { combineReducers } from 'redux'
 
 export const rootReducer = combineReducers({
-  
+  questions,
+  currentRound,
+  currentQuestion,
+  score,
+  lives,
+  name,
+  leaderboard,
+  gameStatus,
 })
