9358210efe07acc327488caa1ec506ed4c453980
Bill Wilke <billwilke2@gmail.com>
2020-07-21 18:00:49 -0600

    Makes initial app wireframe

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,20 @@
 import React  from 'react';
-import logo from './logo.svg';
 import './App.css';
-export default App;
 
 const App = () => {
   return (
-    
+    <main className='App'>
+      <div className='Page'>
+      <section className='header'>
+        <div className='question-number'>Question Number</div>
+        <div className='category'>Category</div>
+        <div className='lives'>Lives</div>
+      </section>
+      <section className='body'>
+        <div className='chalkboard'>Chalkboard</div>
+      </section>
+      </div>
+    </main>
   )
 }
+export default App;
