2b86256fa8aa9c5f701e28e6cd51e5582d3ebb88
Liliana Weimer <lilianaweimer11@gmail.com>
2020-07-21 18:19:34 -0600

    Change leaderboard from table to list

--- src/TeachersPets/TeachersPets.js
+++ src/TeachersPets/TeachersPets.js
@@ -5,28 +5,12 @@ const TeachersPets = () => {
     return (
         <section className='scoreboard'>
             <h2 className='scoreboard-title'>Teacher's Pets:</h2>
-                <tr className='scoreboard-toprow'>
-                    <td>Name</td>
-                    <td>Score</td>
-                </tr>
-                <tr className='scoreboard-playerrow'>
-                    <td>name 1</td>
-                    <td>score 1</td>
-                </tr>
-                <tr>
-                    <td>name 2</td>
-                    <td>score 2</td>
-                </tr>
-                <tr>
-                    <td>name 3</td>
-                    <td>score 4</td>
-                </tr>
-
-            {/* <ul>
-                <li>name 1: score 1</li>
-                <li>name 2: score 2</li>
-                <li>name 2: score 2</li>
-            </ul> */}
+                    <ul className='scoreboard-list'>
+                        <li>Name: Score</li>
+                        <li>Name: Score</li>
+                        <li>Name: Score</li>
+                    </ul>
+            <button>Home</button>
         </section>
     )
 }
