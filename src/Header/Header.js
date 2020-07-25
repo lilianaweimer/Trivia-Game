c02ff5b65ee96f5639357a4e327b5e651ab576f4
Bill Wilke <billwilke2@gmail.com>
2020-07-25 13:07:14 -0600

    Adds space background and styling for form and game over

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,11 +6,13 @@ import './Header.css'
 const Header = (props) => {
   return (
     <header className='header nes-container'>
-      <div className='question-number'>Question {props.questionCounter + 1}</div>
-        <div className='category'>Round {props.currentRound + 1}: {props.question.category}</div>
+      <div className='question-number'>
+        Score: 
+        <p>{props.score}</p>
+       </div>
+        <div className='category'>Round {props.currentRound + 1}: Question {props.questionCounter + 1} <p>{props.question.category}</p></div>
       <div className='lives'>
         Lives: <Lives lives={props.lives} />
-        Score: {props.score}
       </div>
     </header>
   )
--- src/Lives/Lives.js
+++ src/Lives/Lives.js
@@ -2,18 +2,13 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 const Lives = (props) => {
-  console.log('this', props.lives)
   let lives = []
-  // let hearts = lives.fill(<i className='nes-icon heart' />, 0, props.lives)
-  let hearts = () => {
-    for(var j = 0; j < props.lives; j++) {
+  for (var j = 0; j < props.lives; j++) {
       lives.push('')
-    }
   }
-  hearts()
   return (
     <p>{lives.map(heart => {
-      return <i className='nes-icon heart' />
+      return <i className='nes-icon heart is-medium' />
     })}</p>
   )
 }
