319fa12927d7d16eaef3d4336fa57f4ddfc1c3f1
Kyle Wong <kylewong2510@gmail.com>
2020-07-26 20:34:07 -0600

    Display teachersPets

--- src/TeachersPets/TeachersPets.js
+++ src/TeachersPets/TeachersPets.js
@@ -4,11 +4,18 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const TeachersPets = (props) => {
-  // if(props.teachersPets.length) {
+  if(props.teachersPets.length) {
+    console.log(props.teachersPets[0])
+    let pets = props.teachersPets[0].map(pet => {
+      console.log(pet, 'PET')
+      return <li>{pet.initials}: {pet.score}</li>
+    })
+    console.log(pets)
     return (
       <section className='scoreboard'>
         <h2 className='scoreboard-title'>Teacher's Pets:</h2>
         <ul className='scoreboard-list'>
+            {pets}
             {/* <li>{props.teachersPets[0].initials}: {props.teachersPets[0].score}</li>
             <li>{props.teachersPets[1].initials}: {props.teachersPets[1].score}</li>
             <li>{props.teachersPets[2].initials}: {props.teachersPets[2].score}</li> */}
@@ -16,14 +23,14 @@ const TeachersPets = (props) => {
         <Link to='/'><button className='nes-btn'>Home</button></Link>
       </section>
     )
-  // } else {
-  //   return (
-  //     <section className='scoreboard'>
-  //       <h2>No high scores</h2>
-  //       <Link to='/'><button className='nes-btn'>Home</button></Link>
-  //     </section>
-  //   )
-  // }
+  } else {
+    return (
+      <section className='scoreboard'>
+        <h2>No high scores</h2>
+        <Link to='/'><button className='nes-btn'>Home</button></Link>
+      </section>
+    )
+  }
 }
 
 const mapStateToProps = ({ setTeachersPets }) => ({
