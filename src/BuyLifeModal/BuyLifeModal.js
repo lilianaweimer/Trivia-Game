aaaf5674823fd36d0f9fbbffa600e033d1662df5
Kyle Wong <kylewong2510@gmail.com>
2020-07-28 14:53:17 -0600

    Style modal

--- src/BuyLifeModal/BuyLifeModal.js
+++ src/BuyLifeModal/BuyLifeModal.js
@@ -3,11 +3,11 @@ import './BuyLifeModal.css'
 
 const BuyLifeModal = (props) => {
   return (
-    <section>
+    <section className='buy-life-modal'>
       <h2>Buy Life?</h2>
       <p>Buying an extra life costs 300 points.  Do you wish to continue?</p>
-      <button className='nes-css' onClick={props.buyLife}>Yes</button>
-      <button className='nes-css' onClick={props.toggleBuyLifeModal}>No</button>
+      <button className='nes-css yes-life' onClick={props.buyLife}>Yes</button>
+      <button className='nes-css no-life' onClick={props.toggleBuyLifeModal}>No</button>
     </section>
   )
 }
